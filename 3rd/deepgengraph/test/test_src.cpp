52ae54434c9826d281d23ca40cd78f14013d5daa
Hebe.Y <1531255491@qq.com>
2026-09-01 21:41:38 +0800

    [dev] 临时添加newloc构造

--- 3rd/deepgengraph/test/test_src.cpp
+++ 3rd/deepgengraph/test/test_src.cpp
@@ -257,6 +257,34 @@ frisk::KernelConfig* frisk::GetKernelConfig() {
   return &cfg;
 }
 
+
+mlir::Location frisk::GetNewLoc(mlir::Location origLoc, mlir::Operation* targetOp){
+  // 假设 op 是已有的 Operation*，newOpName 是你要追加的信息
+  mlir::MLIRContext *ctx = targetOp->getContext();
+  llvm::StringRef extraInfo = targetOp->getOperationName().getStringRef();
+  mlir::Location newLoc = origLoc; // 默认降级为原 Loc
+
+  // 1. 匹配并提取 FileLineColLoc
+  if (auto fileLoc = llvm::dyn_cast<mlir::FileLineColLoc>(origLoc)) {
+      // 获取原 fileName 字符串
+      llvm::StringRef origFileName = fileLoc.getFilename().getValue();
+
+      // 拼接新的 fileName
+      std::string newFileName = (origFileName + "_" + extraInfo).str();
+
+      // 2. 重新构建包含新 fileName 的 FileLineColLoc
+      newLoc = mlir::FileLineColLoc::get(
+          mlir::StringAttr::get(ctx, newFileName),
+          fileLoc.getLine(),
+          fileLoc.getColumn()
+      );
+  }
+
+  // 3. 将新 Loc 赋值给目标 Op（例如通过 OpBuilder 创建时传入，或直接 setLoc）
+  // targetOp->setLoc(newLoc);
+  return newLoc;
+}
+
 int readDeepgenGraphIRAndConvertToFriskPipeline(int argc, char ** argv) {
   mlir::DialectRegistry registry;
   mlir::registerAllExtensions(registry);
